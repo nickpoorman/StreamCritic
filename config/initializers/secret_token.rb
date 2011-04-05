761bef6155937b3b58ea223348e9729c214f14e4
Nicholas Poorman <nickpoorman@gmail.com>
2011-04-04 22:56:16 -0400

    Groundwork In Progress

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreamCritic::Application.config.secret_token = '7f19cbcf05702d5fdbffba5cabee8b43efbbe99ec069f3bb50c4f27660a9e4633dfb000d8a9496b9dd6d8276bd0963e933b682ecb660bcd5351154977de0cc7a'

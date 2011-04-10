1956fdabdf42c4ff1f3933bd62e4335e0db471ed
Nicholas Poorman <nickpoorman@gmail.com>
2011-04-10 00:27:09 -0400

    Added comment textbox to show

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,9 +3,18 @@ class Tag
   include Mongoid::Timestamps
 
   field :comment
-  field :time
+  field :time_min
+  field :time_sec
+
+  attr_accessible :url
+  before_validation :set_time_if_blank
 
   validates_presence_of :comment, :time
 
   embedded_in :video, :inverse_of => :tags
+
+  private
+    def set_time_if_blank
+      self.time = '00:00'
+    end
 end

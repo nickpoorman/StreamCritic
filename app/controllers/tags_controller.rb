495767cadb888a39f401abbc9d2f4a55121d2cd7
Nicholas Poorman <nickpoorman@gmail.com>
2011-04-11 15:48:55 -0400

    Did some stuff

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,9 @@ class TagsController < ApplicationController
   def create
     @video = Video.find(params[:video_id])
     @tag = @video.tags.create!(params[:tag])
-    redirect_to @video, :notice => "Tag added!"
+    respond_to do |format|
+      format.html { redirect_to @video, :notice => "Tag added!" }
+      format.js
+    end
   end
 end

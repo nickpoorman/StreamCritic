d5f03cf10baa0dfe8f8ba5cd16e751eeac7e7c98
Nicholas Poorman <nickpoorman@gmail.com>
2011-04-11 19:35:20 -0400

    Added in a progress bar to the video

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 
   def create
     @video = Video.find(params[:video_id])
-    @tag = @video.tags.create!(params[:tag])
+    @tag_enum = @video.tags.create!(params[:tag])
     respond_to do |format|
       format.html { redirect_to @video, :notice => "Tag added!" }
       format.js

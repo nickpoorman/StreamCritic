f9bdbc744f4e8e6341e4d0d11e0d33660adb4d3b
Nicholas Poorman <nickpoorman@gmail.com>
2011-04-12 20:30:54 -0400

    Changed domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
   protect_from_forgery
+  
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.Criticstream.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+  before_filter :login_required, :only => [:new, :create]
 
   def create
     @video = Video.find(params[:video_id])
